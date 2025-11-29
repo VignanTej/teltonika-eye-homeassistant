60e158b4338626afed44f1492bd508154a706b92
Vignan Tej <vignan@tezsolutions.in>
2025-11-29 13:23:46 +0530

    fix: Use template component in AUTO_LOAD instead of text - Change AUTO_LOAD from text to template - Template component includes TemplateText support for dynamic MAC configuration - Fixes linker errors for template text entities

--- esphome/components/teltonika_ble/__init__.py
+++ esphome/components/teltonika_ble/__init__.py
@@ -3,7 +3,7 @@ import esphome.config_validation as cv
 from esphome.components import esp32_ble_tracker
 from esphome.const import CONF_ID, CONF_TIMEOUT
 
-AUTO_LOAD = ["sensor", "binary_sensor", "text"]
+AUTO_LOAD = ["sensor", "binary_sensor", "template"]
 
 CONF_DISCOVER = "discover"
 
